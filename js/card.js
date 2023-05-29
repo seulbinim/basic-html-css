1b5c15841eb37b7f36511f9e5db7551b526a16c4
seulbinim <seulbinim@gmail.com>
2023-05-29 12:43:48 +0900

    main 브랜치 정리

--- js/card.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('.flip-button');
-const card = document.querySelector('.card');
-
-button.addEventListener('click', () => {
-  card.classList.toggle('is-flipped');
-});
--- js/peak-time.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('.voting-result-button');
-const results = document.querySelectorAll('.voting-result');
-
-button.addEventListener("click", () => {
-  results.forEach(result => {
-    result.removeAttribute('hidden');
-  });
-});
